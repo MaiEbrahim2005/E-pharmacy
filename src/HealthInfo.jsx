7dcac0f7ee4a0b3fe0545df1a99ccd89e4d10b2f
Mai Ebrahim <maiisaad125@gmail.com>
2025-12-09 19:24:49 +0200

    fix import

--- src/HealthInfo.jsx
+++ src/HealthInfo.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./HealthInfo.css";
+import "./Healthinfo.css";
 
 const HealthInfo = () => {
   const [hasChronic, setHasChronic] = useState("");
